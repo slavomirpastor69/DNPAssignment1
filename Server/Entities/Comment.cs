565e3b012654d68ac50e0b0af9dcfa1e6f03c98a
slavomirpastor69 <361691@via.dk>
2026-09-06 12:19:53 +0200

    th rest

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int PostId { get; set; }
+    public int AuthorId { get; set; }
+}
